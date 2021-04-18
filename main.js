83422e0f703d8f3fa10ef7da1ad511a7c8ca41a9
Jai Relan <jairelan.2005@gmail.com>
2021-04-18 21:47:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,28 +19,43 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
         var websites = [
             'https://www.aarp.org',
-            'https://abcnews.go.com/',
-            'https://www.abridgenews.com/',
+            'https://abcnews.go.com',
+            'https://www.abridgenews.com',
             'https://www.aim.org',
-            'https://www.aclu.org/',
-            'https://www.ajplus.net/',
+            'https://www.aclu.org',
+            'https://www.ajplus.net',
             'https://www.aljazeera.com',
-            'https://www.alternet.org/',
-            'https://www.conservative.org/',
-            'https://www.aei.org/',
-            'https://amgreatness.com/',
-            'https://spectator.org/',
+            'https://www.alternet.org',
+            'https://www.conservative.org',
+            'https://www.aei.org',
+            'https://amgreatness.com',
+            'https://spectator.org',
         ]
 
+        var bias = {
+            'https://www.aarp.org' : 'center',
+            'https://abcnews.go.com' : 'right',
+            'https://www.abridgenews.com': 'right',
+            'https://www.aim.org': 'right',
+            'https://www.aclu.org': 'right',
+            'https://www.ajplus.net': 'right',
+            'https://www.aljazeera.com': 'right',
+            'https://www.alternet.org': 'left',
+            'https://www.conservative.org': 'right',
+            'https://www.aei.org': 'right',
+            'https://amgreatness.com': 'right',
+            'https://spectator.org': 'right',
+        }
         var known_url = websites.includes(origin);
 
         // alert(known_url)
 
         if (known_url == true){
-            alert("Namaste")
+            var bias_val = bias[origin]
+            alert(origin + " is known to be an organisation with views leaning towards the " + bias_val)
         }
-        else{
-            alert("Nuh uh")
+        else if (known_url == false){
+            alert("Sorry, but we haven't been able to conclude the bias on " + origin)
         }
 
     });
