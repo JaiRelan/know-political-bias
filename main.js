8e9c95cc4916491a6239499e4d1d66f11801fe75
Jai Relan <jairelan.2005@gmail.com>
2021-04-19 15:25:19 +0530

    Removed the random other code

--- main.js
+++ main.js
@@ -59,23 +59,4 @@ chrome.browserAction.onClicked.addListener(function (tab) {
         }
 
     });
-
-
-    // function search(target) {
-
-    //     var name = document.getElementById("searchForm").elements["searchItem"].value;
-    //     var pattern = name.toLowerCase();
-    //     var targetId = target;
-
-    //     var divs = document.getElementsByClassName("col-md-2");
-    //     for (var i = 0; i < divs.length; i++) {
-    //         var para = divs[i].getElementsByTagName("p");
-    //         var index = para[0].innerText.toLowerCase().indexOf(pattern);
-    //         if (index != -1) {
-    //             targetId = divs[i].parentNode.id;
-    //             document.getElementById(targetId).scrollIntoView();
-    //             break;
-    //         }
-    //     }
-    // }
 });
